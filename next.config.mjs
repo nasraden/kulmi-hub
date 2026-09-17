bb5a08b99d68153f222982d0250ea6720ac2180f
Nasra Adan <adann364@gmail.com>
2026-09-17 20:11:54 +0300

    push all nextjs project files to repository

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+
+eslint: {
+  ignoreDuringBuilds: true,
+},
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
